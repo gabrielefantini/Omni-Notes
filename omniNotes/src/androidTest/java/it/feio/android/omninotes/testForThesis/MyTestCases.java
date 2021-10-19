a2ca8b867a67ecb1292755371593751c58168427
gabrielefantini <gabrielefantini97@gmail.com>
2021-10-19 14:45:47 +0200

    Fixed delay problem given by swipe left animation

--- omniNotes/src/androidTest/java/it/feio/android/omninotes/testForThesis/MyTestCases.java
+++ omniNotes/src/androidTest/java/it/feio/android/omninotes/testForThesis/MyTestCases.java
@@ -1,6 +1,7 @@
 package it.feio.android.omninotes.testForThesis;
 
 
+import static androidx.test.espresso.Espresso.onData;
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.Espresso.pressBack;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -8,24 +9,32 @@ import static androidx.test.espresso.action.ViewActions.swipeLeft;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.contrib.DrawerMatchers.isClosed;
+import static androidx.test.espresso.contrib.DrawerMatchers.isOpen;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withChild;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 
+import static org.hamcrest.Matchers.anything;
+
 import android.os.SystemClock;
+import android.view.Gravity;
 
 import androidx.test.core.app.ActivityScenario;
 import androidx.test.espresso.action.GeneralLocation;
 import androidx.test.espresso.action.GeneralSwipeAction;
 import androidx.test.espresso.action.Press;
 import androidx.test.espresso.action.Swipe;
+import androidx.test.espresso.contrib.DrawerActions;
+import androidx.test.espresso.contrib.NavigationViewActions;
 import androidx.test.espresso.contrib.RecyclerViewActions;
 import androidx.test.espresso.intent.Intents;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.filters.LargeTest;
 
+import org.hamcrest.Matchers;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -37,13 +46,12 @@ import it.feio.android.omninotes.R;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class InsertNoteTestCase {
+public class MyTestCases {
     private static final String NOTE_TITLE = "Titolo di prova";
     private static final String NOTE_CONTENT = "Contenuto di prova";
 
-
     @Test
-    public void insertNewNote(){
+    public void testInsertNote(){
         /*=======================
            1-Click fab
            2-Verify the UI
@@ -56,14 +64,14 @@ public class InsertNoteTestCase {
         //  SETUP
         ActivityScenario activityScenario = ActivityScenario.launch(MainActivity.class);
 
-        //click on fab button
+        //Click on fab button
         onView(withId(R.id.fab_expand_menu_button)).perform(click());
         onView(withId(R.id.fab_note)).perform(click());
-        //check if navigation on another fragment has happened
+        //Check if navigation on another fragment has happened
         onView(withId(R.id.detail_root)).check(matches(isDisplayed()));
         onView(withId(R.id.detail_tile_card)).check(matches(isDisplayed()));
         onView(withId(R.id.detail_content_card)).check(matches(isDisplayed()));
-        //inserting new title
+        //Inserting new title
         onView(withId(R.id.detail_title)).perform(typeText(NOTE_TITLE));
         onView(withId(R.id.detail_content)).perform(typeText(NOTE_CONTENT));
 
@@ -75,14 +83,36 @@ public class InsertNoteTestCase {
     }
 
     @Test
-    public void deleteNote(){
+    public void testArchiveNote(){
+        /*=======================
+           1-Swipe Left
+           2-Click drawer
+           3-Verify that archive section is appeared
+           4-Click on Archive
+           5-Verify that the note archived is displayed
+         ========================*/
+
         //  SETUP
         ActivityScenario activityScenario = ActivityScenario.launch(MainActivity.class);
 
+        //Swipe left
         onView(withId(R.id.list)).perform(RecyclerViewActions.actionOnItemAtPosition(0, swipeLeft()));
-        SystemClock.sleep(500);
-        onView(withText(NOTE_TITLE)).check(doesNotExist());
-
+        //Open drawer two times to avoid animation of swipe left
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isOpen(Gravity.LEFT)))
+                .perform(DrawerActions.close());
+        onView(withId(R.id.drawer_layout))
+                .check(matches(isClosed(Gravity.LEFT)))
+                .perform(DrawerActions.open());
+        //Click on Archive on the drawer menu
+        onView(withText("Archive")).check(matches(isDisplayed()));
+        onView(withText("Archive")).perform(click());
+        //Check if actually the note has been archived
+        onView(withText(NOTE_TITLE)).check(matches(isDisplayed()));
+        onView(withText(NOTE_CONTENT)).check(matches(isDisplayed()));
     }
 
 }
